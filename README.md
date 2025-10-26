fbe8e3e0826e4219120bc089c9ff4278b3859f2f
Jordan <hello@jyodann.com>
2025-10-26 23:03:45 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## latexify
+
+A project that takes in a Math Equation and changes it to latex using Chrome's Prompt API.
+
+## Running this extension
+
+1. Clone this repository.
+1. Run `npm install` in the project directory.
+1. Run `npm run build` in the project directory to build the extension.
+1. Load the newly created `dist` directory in Chrome as an [unpacked extension](https://developer.chrome.com/docs/extensions/get-started/tutorial/hello-world#load-unpacked).
+1. Click the extension icon.
+1. Interact with the Prompt API in the sidebar.
---
+++ sidepanel/index.js
@@ -0,0 +1,129 @@
+/* global LanguageModel */
+
+import DOMPurify from 'dompurify';
+import { marked } from 'marked';
+
+const inputPrompt = document.body.querySelector('#input-prompt');
+const buttonPrompt = document.body.querySelector('#button-prompt');
+const buttonReset = document.body.querySelector('#button-reset');
+const elementResponse = document.body.querySelector('#response');
+const elementLoading = document.body.querySelector('#loading');
+const elementError = document.body.querySelector('#error');
+const sliderTemperature = document.body.querySelector('#temperature');
+const sliderTopK = document.body.querySelector('#top-k');
+const labelTemperature = document.body.querySelector('#label-temperature');
+const labelTopK = document.body.querySelector('#label-top-k');
+
+const inputImage = document.body.querySelector("#equation")
+
+let session;
+
+async function runPrompt(prompt, params) {
+    const session = await LanguageModel.create({
+      expectedInputs: [{ type: "image" }],
+    });
+    const prompt2 =
+      "Write the latex for this equation";
+
+    console.log(inputImage.files)
+    const stream = session.promptStreaming([
+      {
+        role: "user",
+        content: [
+          { type: "text", value: prompt2 },
+          { type: "image", value: inputImage.files[0] },
+        ],
+      },
+    ]);
+    let chunks = ""
+    for await (const chunk of stream) { 
+      chunks += chunk;
+    }
+    return chunks
+}
+
+
+
+async function reset() {
+  if (session) {
+    session.destroy();
+  }
+  session = null;
+}
+
+async function initDefaults() {
+  const defaults = await LanguageModel.params();
+  console.log('Model default:', defaults);
+  if (!('LanguageModel' in self)) {
+    showResponse('Model not available');
+    return;
+  }
+  sliderTemperature.value = defaults.defaultTemperature;
+  // Pending https://issues.chromium.org/issues/367771112.
+  // sliderTemperature.max = defaults.maxTemperature;
+  if (defaults.defaultTopK > 3) {
+    // limit default topK to 3
+    sliderTopK.value = 3;
+    labelTopK.textContent = 3;
+  } else {
+    sliderTopK.value = defaults.defaultTopK;
+    labelTopK.textContent = defaults.defaultTopK;
+  }
+  sliderTopK.max = defaults.maxTopK;
+  labelTemperature.textContent = defaults.defaultTemperature;
+}
+
+initDefaults();
+
+buttonReset.addEventListener('click', () => {
+  hide(elementLoading);
+  hide(elementError);
+  hide(elementResponse);
+  reset();
+  buttonReset.setAttribute('disabled', '');
+});
+
+sliderTemperature.addEventListener('input', (event) => {
+  labelTemperature.textContent = event.target.value;
+  reset();
+});
+
+sliderTopK.addEventListener('input', (event) => {
+  labelTopK.textContent = event.target.value;
+  reset();
+});
+
+buttonPrompt.addEventListener('click', async () => {
+  const prompt = inputPrompt.value.trim();
+  showLoading();
+  const response =await runPrompt(prompt, prompt)
+  showResponse(response)
+});
+
+function showLoading() {
+  buttonReset.removeAttribute('disabled');
+  hide(elementResponse);
+  hide(elementError);
+  show(elementLoading);
+}
+
+function showResponse(response) {
+  hide(elementLoading);
+  show(elementResponse);
+  elementResponse.innerHTML = DOMPurify.sanitize(marked.parse(response));
+}
+
+function showError(error) {
+  show(elementError);
+  hide(elementResponse);
+  hide(elementLoading);
+  elementError.textContent = error;
+}
+
+function show(element) {
+  element.removeAttribute('hidden');
+}
+
+function hide(element) {
+  element.setAttribute('hidden', '');
+}
