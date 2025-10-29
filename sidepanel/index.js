aeac5b435900939f6fc9741734c79f0d9112b0b7
Jordan <hello@jyodann.com>
2025-10-30 01:23:14 +0800

    Removed Certain HTML Elements

--- sidepanel/index.js
+++ sidepanel/index.js
@@ -1,32 +1,21 @@
 /* global LanguageModel */
-
-import DOMPurify from 'dompurify';
-import { marked } from 'marked';
-
-const inputPrompt = document.body.querySelector('#input-prompt');
 const buttonPrompt = document.body.querySelector('#button-prompt');
 const buttonReset = document.body.querySelector('#button-reset');
 const elementResponse = document.body.querySelector('#response');
 const elementLoading = document.body.querySelector('#loading');
 const elementError = document.body.querySelector('#error');
-const sliderTemperature = document.body.querySelector('#temperature');
-const sliderTopK = document.body.querySelector('#top-k');
-const labelTemperature = document.body.querySelector('#label-temperature');
-const labelTopK = document.body.querySelector('#label-top-k');
 const buttonDownload = document.body.querySelector('#button-download');
 const inputImage = document.body.querySelector("#equation")
 
 let session;
 
 async function runPrompt(prompt, params) {
-    console.log(await LanguageModel.availability());
     const session = await LanguageModel.create({
       expectedInputs: [{ type: "image" }],
     });
     const prompt2 =
-      "Write the latex equivalent:";
+      "Output the raw latex equivalent";
 
-    console.log(inputImage.files)
     const stream = session.promptStreaming([
       {
         role: "user",
@@ -59,19 +48,19 @@ async function initDefaults() {
     showResponse('Model not available');
     return;
   }
-  sliderTemperature.value = defaults.defaultTemperature;
-  // Pending https://issues.chromium.org/issues/367771112.
-  // sliderTemperature.max = defaults.maxTemperature;
-  if (defaults.defaultTopK > 3) {
-    // limit default topK to 3
-    sliderTopK.value = 3;
-    labelTopK.textContent = 3;
-  } else {
-    sliderTopK.value = defaults.defaultTopK;
-    labelTopK.textContent = defaults.defaultTopK;
-  }
-  sliderTopK.max = defaults.maxTopK;
-  labelTemperature.textContent = defaults.defaultTemperature;
+  // sliderTemperature.value = defaults.defaultTemperature;
+  // // Pending https://issues.chromium.org/issues/367771112.
+  // // sliderTemperature.max = defaults.maxTemperature;
+  // if (defaults.defaultTopK > 3) {
+  //   // limit default topK to 3
+  //   sliderTopK.value = 3;
+  //   labelTopK.textContent = 3;
+  // } else {
+  //   sliderTopK.value = defaults.defaultTopK;
+  //   labelTopK.textContent = defaults.defaultTopK;
+  // }
+  // sliderTopK.max = defaults.maxTopK;
+  // labelTemperature.textContent = defaults.defaultTemperature;
 }
 
 initDefaults();
@@ -84,24 +73,17 @@ buttonReset.addEventListener('click', () => {
   buttonReset.setAttribute('disabled', '');
 });
 
-sliderTemperature.addEventListener('input', (event) => {
-  labelTemperature.textContent = event.target.value;
-  reset();
-});
 
-sliderTopK.addEventListener('input', (event) => {
-  labelTopK.textContent = event.target.value;
-  reset();
-});
 
 buttonDownload.addEventListener('click', async (event) => {
-  const session = await LanguageModel.create({
-  monitor(m) {
-    m.addEventListener('downloadprogress', (e) => {
-      console.log(`Downloaded ${e.loaded * 100}%`);
-    });
-  },
-});
+    await LanguageModel.availability();
+    const session = await LanguageModel.create({
+    monitor(m) {
+      m.addEventListener('downloadprogress', (e) => {
+        console.log(`Downloaded ${e.loaded * 100}%`);
+      });
+    },
+  });
 });
 
 buttonPrompt.addEventListener('click', async () => {
@@ -121,7 +103,11 @@ function showLoading() {
 function showResponse(response) {
   hide(elementLoading);
   show(elementResponse);
-  elementResponse.innerHTML = DOMPurify.sanitize(marked.parse(response));
+
+  response = response.replace("```latex", "")
+  response = response.replace("```", "")
+
+  elementResponse.innerHTML = response
 }
 
 function showError(error) {
